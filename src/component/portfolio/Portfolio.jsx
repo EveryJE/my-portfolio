45fee53289ce74c3d4b22c99c0176ec076a641ca
Kofi Gyan <kgyan19lf@outlook.com>
2023-09-30 21:24:46 +0100

    default portfolio

--- src/component/portfolio/Portfolio.jsx
+++ src/component/portfolio/Portfolio.jsx
@@ -24,7 +24,7 @@ import {
 
 
 export const Portfolio = ({section}) => {
-  const [selectedItem, setSelectedItem] = useState(0);
+  const [selectedItem, setSelectedItem] = useState(2);
   const [selectedData, setSelectedData] = useState([]);
 
   useEffect(() => {
